fc628b64234c3707075c86465d4230135562488d
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2025-04-15 16:37:08 -0500

    Feature: add corsheaders in settings

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'calculations',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # URL de tu frontend en desarrollo
+    "http://localhost:5173", # URL de tu frontend en desarrollo
+]
+
+CORS_ALLOW_CREDENTIALS = True
