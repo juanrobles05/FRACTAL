8333c2d706e14ac5640fdbe23ce0a6ec21cfe0a8
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2025-05-20 14:53:37 -0500

    Feature: creation of the Secante method

--- Backend/calculations/urls.py
+++ Backend/calculations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import test_calculations, calculate_regla_falsa
+from .views import test_calculations, calculate_regla_falsa, calculate_secant
 
 urlpatterns = [
     path('test/', test_calculations, name='test_calculations'),
-    path('reglaFalsa/', calculate_regla_falsa, name='calculate_regla_falsa')
+    path('reglaFalsa/', calculate_regla_falsa, name='calculate_regla_falsa'),
+    path('secante/', calculate_secant, name='calculate_secant')
 ]
--- Backend/calculations/views.py
+++ Backend/calculations/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .methods.ReglaFalsa import false_position_method
+from .methods.Secante import secant_method
 
 @api_view(['GET'])
 def test_calculations(request):
@@ -24,6 +25,32 @@ def calculate_regla_falsa(request):
 
         # Llamar a la función del método de Regla Falsa.
         results = false_position_method(function_text, a, b, tol, max_count)
+
+        return Response(results, status=200)
+
+    except ValueError as e:
+        return Response({"error": str(e)}, status=400)
+    except Exception as e:
+        return Response({"error": "Error inesperado: " + str(e)}, status=500)
+
+@api_view(['POST'])
+def calculate_secant(request):
+
+    try:
+        # Leer los datos enviados desde el frontend.
+        data = request.data
+        function_text = data.get('function_text')
+        x0 = float(data.get('x0'))
+        x1 = float(data.get('x1'))
+        tol = float(data.get('tol'))
+        max_count = int(data.get('max_count'))
+
+        # Validar que los datos sean correctos.
+        if not function_text or x0 is None or x1 is None or tol is None or max_count is None:
+            return Response({"error": "Faltan parámetros"}, status=400)
+
+        # Llamar a la función del método de Regla Falsa.
+        results = secant_method(function_text, x0, x1, tol, max_count)
 
         return Response(results, status=200)
 
