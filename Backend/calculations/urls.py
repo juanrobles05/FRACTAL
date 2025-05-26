20e3393597b19030449d9ec714c01b94c81f8f57
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2025-05-25 22:18:44 -0500

    Feature: add the view and url to the PuntoFijo method

--- Backend/calculations/urls.py
+++ Backend/calculations/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import test_calculations, calculate_regla_falsa, calculate_secant, calculate_multiple_roots, calculate_biseccion
-
+from .views import calculate_fixed_point
 urlpatterns = [
     path('test/', test_calculations, name='test_calculations'),
     path('reglaFalsa/', calculate_regla_falsa, name='calculate_regla_falsa'),
     path('secante/', calculate_secant, name='calculate_secant'),
     path('raicesMultiples/', calculate_multiple_roots, name='calculate_multiple_roots'),
     path('biseccion/', calculate_biseccion, name='calculate_biseccion'),
+    path('puntoFijo/', calculate_fixed_point, name='calculate_fixed_point'),
 ]
--- Backend/calculations/views.py
+++ Backend/calculations/views.py
@@ -4,6 +4,7 @@ from .methods.ReglaFalsa import false_position_method
 from .methods.Secante import secant_method
 from .methods.RaicesMultiples import multiple_roots_method
 from .methods.Biseccion import bisection_method
+from .methods.PuntoFijo import fixed_point_method
 
 @api_view(['GET'])
 def test_calculations(request):
@@ -106,6 +107,32 @@ def calculate_biseccion(request):
 
         # Llamar a la función del método de Bisección.
         results = bisection_method(function_text, a, b, tol, max_count)
+
+        return Response(results, status=200)
+
+    except ValueError as e:
+        return Response({"error": str(e)}, status=400)
+    except Exception as e:
+        return Response({"error": "Error inesperado: " + str(e)}, status=500)
+
+
+@api_view(['POST'])
+def calculate_fixed_point(request):
+    try:
+        # Leer los datos enviados desde el frontend.
+        data = request.data
+        function_text = data.get('function_text')
+        gfunction_text = data.get('gfunction_text')
+        x0 = float(data.get('x0'))
+        tol = float(data.get('tol'))
+        max_count = int(data.get('max_count'))
+
+        # Validar que los datos sean correctos.
+        if not function_text or gfunction_text is None or x0 is None or tol is None or max_count is None:
+            return Response({"error": "Faltan parámetros"}, status=400)
+
+        # Llamar a la función del método de Punto Fijo.
+        results = fixed_point_method(function_text, gfunction_text, x0, tol, max_count)
 
         return Response(results, status=200)
 
