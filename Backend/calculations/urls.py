2aea3c137b778c96795d1cc644765d23778d0287
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2025-05-28 07:57:40 -0500

    Feature: creation of the Jacobi method

--- Backend/calculations/urls.py
+++ Backend/calculations/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import test_calculations, calculate_regla_falsa, calculate_secant, calculate_multiple_roots, calculate_biseccion
-from .views import calculate_fixed_point, calculate_newton
+from .views import calculate_fixed_point, calculate_newton, calculate_jacobi
 urlpatterns = [
     path('test/', test_calculations, name='test_calculations'),
     path('reglaFalsa/', calculate_regla_falsa, name='calculate_regla_falsa'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('biseccion/', calculate_biseccion, name='calculate_biseccion'),
     path('puntoFijo/', calculate_fixed_point, name='calculate_fixed_point'),
     path('newton/', calculate_newton, name='calculate_newton'),
+    path('cap2/jacobi/', calculate_jacobi, name='calculate_jacobi'),
 ]
--- Backend/calculations/views.py
+++ Backend/calculations/views.py
@@ -6,6 +6,7 @@ from .methods.RaicesMultiples import multiple_roots_method
 from .methods.Biseccion import bisection_method
 from .methods.PuntoFijo import fixed_point_method
 from .methods.Newton import newton_method
+from .methods.cap2.Jacobi import jacobi_method
 
 @api_view(['GET'])
 def test_calculations(request):
@@ -165,4 +166,34 @@ def calculate_newton(request):
     except ValueError as e:
         return Response({"error": str(e)}, status=400)
     except Exception as e:
+        return Response({"error": "Error inesperado: " + str(e)}, status=500)
+
+@api_view(['POST'])
+def calculate_jacobi(request):
+    try:
+        # Leer los datos enviados desde el frontend.
+        data = request.data
+        matrixA = data.get('matrix')
+        vectorB = data.get('vector_b')
+        vectorX0 = data.get('vector_x0')
+        norm_type = float(data.get('norm'))
+        tol = float(data.get('tol'))
+        max_count = int(data.get('max_count'))
+
+        print(data)
+
+        # Validar que los datos sean correctos.
+        if not matrixA or not vectorB or not vectorX0 or tol is None or max_count is None:
+            return Response({"error": "Faltan parámetros"}, status=400)
+
+        # Llamar a la función del método de Jacobi.
+        results = jacobi_method(matrixA, vectorB, vectorX0, tol, max_count, norm_type)
+
+        return Response(results, status=200)
+
+    except ValueError as e:
+        print(e)
+        return Response({"error": str(e)}, status=400)
+    except Exception as e:
+        print(e)
         return Response({"error": "Error inesperado: " + str(e)}, status=500)
