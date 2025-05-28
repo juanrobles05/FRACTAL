31de63b0fee947be743ecdbf8d430c61fecd9def
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2025-05-28 14:53:40 -0500

    Feature: creation of the Sor method

--- Backend/calculations/views.py
+++ Backend/calculations/views.py
@@ -8,6 +8,7 @@ from .methods.PuntoFijo import fixed_point_method
 from .methods.Newton import newton_method
 from .methods.cap2.Jacobi import jacobi_method
 from .methods.cap2.GaussSeidel import gaussSeidel_method
+from .methods.cap2.Sor import sor_method
 
 @api_view(['GET'])
 def test_calculations(request):
@@ -219,4 +220,34 @@ def calculate_gaussSeidel(request):
     except ValueError as e:
         return Response({"error": str(e)}, status=400)
     except Exception as e:
+        return Response({"error": "Error inesperado: " + str(e)}, status=500)
+
+@api_view(['POST'])
+def calculate_sor(request):
+    try:
+         # Leer los datos enviados desde el frontend.
+        data = request.data
+        matrixA = data.get('matrix')
+        vectorB = data.get('vector_b')
+        vectorX0 = data.get('vector_x0')
+        norm_type = float(data.get('norm'))
+        w = float(data.get('w'))
+        tol = float(data.get('tol'))
+        max_count = int(data.get('max_count'))
+
+        # Validar que los datos sean correctos.
+        if not matrixA or not vectorB or not vectorX0 or tol is None or max_count is None:
+            return Response({"error": "Faltan parámetros"}, status=400)
+
+        # Llamar a la función del método de Jacobi.
+        results = sor_method(matrixA, vectorB, vectorX0, tol, max_count, norm_type, w)
+
+        print(results)
+        return Response(results, status=200)
+
+    except ValueError as e:
+        print(e)
+        return Response({"error": str(e)}, status=400)
+    except Exception as e:
+        print(e)
         return Response({"error": "Error inesperado: " + str(e)}, status=500)
